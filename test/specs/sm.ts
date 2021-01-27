63094511656297344d2db2fa3dd3d0f0c50c1040
Vorkoff33 <69457819+Vorkoff33@users.noreply.github.com>
2021-01-27 21:28:02 +0200

    2

--- test/specs/sm.ts
+++ test/specs/sm.ts
@@ -2,7 +2,7 @@ describe('Website', function () {
     it('should be alive', function () {
         browser.url(`https://www.google.com/intl/ru/photos/about/`)
         const img = $('img[src="https://www.google.com/photos/about/static/images/logos/logo_photos_192px.svg"]')
-       if(img.isExisting())
+       if(!img.isExisting())
        {
         throw new Error('Website should be opened, and logo displayed');
        } 
--- test/specs/smoke.ts
+++ test/specs/smoke.ts
@@ -2,9 +2,10 @@ describe('Website', function () {
     it('should be alive', function () {
         browser.url(`/`)
         const img = $('img[src="https://demo.litecart.net/images/logotype.png"]')
-       if(img.isExisting())
+       if(!img.isExisting())
        {
         throw new Error('Website should be opened, and logo displayed');
        } 
     })
 })
+
